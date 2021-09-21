c84cc048074ad374c3cb6839abd495742ba30784
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2021-09-21 10:59:08 +0200

    Added tsconfig
    - declaration files are now generated
    - Enabled strict mode - fixed a legitimate return type error
    - fixed 4 bogus problems invented by tsc

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
-type Emit<T> = undefined extends T
+type Emit<T> = void extends T
     ? (event?: T) => void
     : (event: T) => void
 type Listener<T> = (event: T) => any
 type Dispose = () => void
-export type Subscribe<T> = (listener: Listener<T>, sync?: boolean | undefined) => Dispose
+export type Subscribe<T> = (listener: Listener<T>, sync?: boolean|void) => Dispose
 
 export function event<T = any>(): [Emit<T>, Subscribe<T>] {
     const listeners = new Set<(event: T) => any>()
     const asyncListeners = new Set<(event: T) => any>()
     return [
-        (event: T) => {
+        ((event: T) => {
             listeners.forEach(listener => listener(event))
             queueMicrotask(() => {
                 asyncListeners.forEach(listener => listener(event))
             })
-        },
-        (listener, sync) => {
+        }) as Emit<T>,
+        (listener: Listener<T>, sync?: boolean|void) => {
             // Passing the same callback multiple times should result in multiple independent entries.
             const entry = (e: T) => listener(e)
             const queue = sync ? listeners : asyncListeners
             queue.add(entry)
-            return () => queue.delete(entry)
+            return () => { queue.delete(entry) }
         }
     ]
 }
